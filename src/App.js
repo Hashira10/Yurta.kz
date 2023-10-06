527b70b7cda272e0b06fb5344c8f00b6bc1ecc9b
tenten <98640569+Hashira10@users.noreply.github.com>
2023-10-06 23:40:51 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
@@ -6,9 +5,41 @@ import Home from './components/Home';
 import About from './components/About';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ApartmentList from './components/ApartmentList';
+import ApartmentDetail from './components/ApartmentDetail';
+
+
+import React, { useEffect, useState } from 'react';
+import { fetchApartments, fetchApartmentById } from './mockApi';
+
 
 
 function App() {
+  // const [apartments, setApartments] = useState([]);
+
+  // useEffect(() => {
+  //   fetchApartments()
+  //     .then((data) => {
+  //       setApartments(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Ошибка при загрузке данных:', error);
+  //     });
+  // }, []);
+
+  const [apartments, setApartments] = useState([]);
+  const [selectedApartmentId, setSelectedApartmentId] = useState(null); // Инициализация идентификатора квартиры
+
+  useEffect(() => {
+    fetchApartments()
+      .then((data) => {
+        setApartments(data);
+      })
+      .catch((error) => {
+        console.error('Ошибка при загрузке данных:', error);
+      });
+  }, []);
+
   return (
     <div>
       <Router>
@@ -16,6 +47,21 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          {/* <Route path="/apartment-list" element={<ApartmentList apartments={apartments} />} /> */}
+          {/* <Route path="/apartment/:id" element={<ApartmentDetail />} /> */}
+          <Route path="/apartment-list"
+            element={
+              <ApartmentList
+                apartments={apartments}
+                onApartmentClick={(id) => setSelectedApartmentId(id)} // Устанавливаем выбранный id
+              />
+            }
+          />
+          <Route
+            path="/apartment/:id"
+            element={<ApartmentDetail apartments={apartments} />}
+          />
+
         </Routes>
       </Router>
       <Footer />
